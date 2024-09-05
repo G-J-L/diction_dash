9e8996629bf5c226ee2daf14024ae892e45401cd
G-J-L <galangjoaquin@gmail.com>
2024-09-05 19:07:47 +0800

    Created several important directories and constants.dart file

---
+++ lib/constants.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+// COLORS
+
+// Orange Colors
+Color kOrangeColor600 = Color(0xFFFF7B00);
+Color kOrangeColor500 = Color(0xFFFF8D21);
+Color kOrangeColor400 = Color(0xFFFFA652);
+Color kOrangeColor300 = Color(0xFFFFB76B);
+Color kOrangeColor200 = Color(0xFFFFCD90);
+Color kOrangeColor100 = Color(0xFFFFF4DF);
+
+// Grays
+Color kGrayColor600 = Color(0xFF545454);
+Color kGrayColor500 = Color(0xFFA6A6A6);
+Color kGrayColor400 = Color(0xFFBFBDBC);
+Color kGrayColor300 = Color(0xFFD9D9D9);
+Color kGrayColor200 = Color(0xFFE9E8E8);
+Color kGrayColor100 = Color(0xFFF8F8F8);
